9934f50949d2361cf67ee521a2adb9eab883fabe
Nathan Tarr <nmtarr@ncsu.edu>
2019-07-26 18:25:39 -0400

    New species-notebooks.  Added species concepts to parameters.sqlite.

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 #NOTE! this is overwritten by some notebooks, so update everywhere, if adding lines.
-sp_id = 'bybcux0'
-summary_name = 'cuckoo'
-gbif_req_id = 'GBIFr7'
-gbif_filter_id = 'GBIFf6'
+sp_id = 'bbtnwx1'
+summary_name = 'green_warbler1'
+gbif_req_id = 'GBIFr12'
+gbif_filter_id = 'GBIFf4'
 ebird_req_id = None
 ebird_filter_id = None
 evaluation = 'eval_gbif1'
 workDir = '/Users/nmtarr/Documents/RANGES/'
-codeDir = '/Users/nmtarr/Code/range_map_evaluation/'
+codeDir = '/Users/nmtarr/Code/occurrence-record-wrangler/'
 inDir = workDir + 'Inputs/'
 outDir = workDir + 'Outputs/'
-default_coordUncertainty = 100
+default_coordUncertainty = 1000
 SRID_dict = {'WGS84': 4326, 'AlbersNAD83': 102008} # Used in file names for output.
 spdb = outDir + sp_id + gbif_req_id + gbif_filter_id + '.sqlite'
