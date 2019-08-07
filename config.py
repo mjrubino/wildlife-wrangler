22375a8f984d74bf6396a0f7d793e6059e302f9d
Nathan Tarr <nmtarr@ncsu.edu>
2019-08-07 11:14:21 -0400

    New notebook.  Deleted evaluations table from sqlite database.  Changed directory name.

--- config.py
+++ config.py
@@ -1,15 +1,14 @@
 #NOTE! this is overwritten by some notebooks, so update everywhere, if adding lines.
-sp_id = 'bbtnwx1'
-summary_name = 'green_warbler1'
-gbif_req_id = 'GBIFr12'
+sp_id = 'bwewax0'
+summary_name = 'wormie'
+gbif_req_id = 'GBIFr14'
 gbif_filter_id = 'GBIFf4'
 ebird_req_id = None
 ebird_filter_id = None
-evaluation = 'eval_gbif1'
-workDir = '/Users/nmtarr/Documents/RANGES/'
+workDir = '/Users/nmtarr/Documents/Occurrences/'
 codeDir = '/Users/nmtarr/Code/occurrence-record-wrangler/'
 inDir = workDir + 'Inputs/'
 outDir = workDir + 'Outputs/'
-default_coordUncertainty = 1000
+default_coordUncertainty = 500
 SRID_dict = {'WGS84': 4326, 'AlbersNAD83': 102008} # Used in file names for output.
 spdb = outDir + sp_id + gbif_req_id + gbif_filter_id + '.sqlite'
