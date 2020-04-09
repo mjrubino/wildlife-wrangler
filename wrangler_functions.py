52489d3bd2468f16cad41ff2c3904aa06937aa9e
Nathan Tarr <nmtarr@ncsu.edu>
2020-04-09 12:00:45 -0400

    Deleted abandoned function.  Notebook run.

--- wrangler_functions.py
+++ wrangler_functions.py
@@ -208,37 +208,6 @@ def MapShapefilePolygons_NC(map_these, title):
     plt.title(title, fontsize=20, pad=-40, backgroundcolor='w')
     return
 
-def download_GAP_range_CONUS2001v1(gap_id, toDir):
-    """
-    Downloads GAP Range CONUS 2001 v1 file and returns path to the unzipped
-    file.  NOTE: doesn't include extension in returned path so that you can
-    specify if you want csv or shp or xml when you use the path.
-    """
-    import sciencebasepy
-    import zipfile
-
-    # Connect
-    sb = sciencebasepy.SbSession()
-
-    # Search for gap range item in ScienceBase
-    gap_id = gap_id[0] + gap_id[1:5].upper() + gap_id[5]
-    item_search = '{0}_CONUS_2001v1 Range Map'.format(gap_id)
-    items = sb.find_items_by_any_text(item_search)
-
-    # Get a public item.  No need to log in.
-    rng =  items['items'][0]['id']
-    item_json = sb.get_item(rng)
-    get_files = sb.get_item_files(item_json, toDir)
-
-    # Unzip
-    rng_zip = toDir + item_json['files'][0]['name']
-    zip_ref = zipfile.ZipFile(rng_zip, 'r')
-    zip_ref.extractall(toDir)
-    zip_ref.close()
-
-    # Return path to range file without extension
-    return rng_zip.replace('.zip', '')
-
 def getGBIFcode(name, rank='species'):
     """
     Returns the GBIF species code for a scientific name.
